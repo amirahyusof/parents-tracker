affcae2523413f2da0e6a197c01d9784bb4179f2
amirah <mirez.data@gmail.com>
2024-11-29 10:19:28 +0800

    commit changes

--- app/(website)/schedule/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from '@/app/(website)/_components/layout';
-
-const ScheduleLayout = ({ children }: { children: React.ReactNode }) => {
-    return <Layout>{children}</Layout>;
-};
-
-export default ScheduleLayout
