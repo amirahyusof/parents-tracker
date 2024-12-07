9d4fe2302159bfe9474e456bdcae3dc9e8b546c3
amirah <mirez.data@gmail.com>
2024-12-07 12:21:20 +0800

    corrected calling props

--- app/interface/activityCard.js
+++ app/interface/activityCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function ActivityCard({userData}){
+export default function ActivityCard({childData}){
   return (
     <section className='p-6'>
       <h1 className='text-2xl mb-4'>Activities</h1>
       <div className='grid grid-cols-2 gap-4'>
-        {userData && (
+        {childData && (
           <div className='card w-40 shadow-xl border-2 border-[#FFDEB4]'>
             <div className='card-body'>
-              <h2 className='text-md font-bold'>{userData.childName}</h2>
-              <p>{userData.activity}</p>
+              <h2 className='text-md font-bold'>{childData.childName}</h2>
+              <p>{childData.activity}</p>
               <div className='card-actions justify-end'>
                 <input type='checkbox' className='toggle toggle-sm hover:bg-blue-200' defaultChecked/>
               </div>
