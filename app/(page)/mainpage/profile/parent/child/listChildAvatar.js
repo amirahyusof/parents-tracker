ce7e87d8fca75a0a677d09659fe9d311b90d0815
amirah <mirez.data@gmail.com>
2024-12-27 00:06:45 +0800

    add if situation - if no child data

--- app/(page)/mainpage/profile/parent/child/listChildAvatar.js
+++ app/(page)/mainpage/profile/parent/child/listChildAvatar.js
@@ -6,33 +6,35 @@ import { useSearchParams } from 'next/navigation';
 export default function ListChildAvatar({childData}) {
   const searchParams = useSearchParams()
   const userId = searchParams.get('userId')
-  // Debug logging
-  console.log('Child Data:', childData);
-
-  useEffect(() => {
-    console.log('Child Data in AvatarChild:', childData);
-  }, [childData])
 
   return (
     <section className='p-2 w-full'>
       <div className='flex gap-2 overflow-x-auto'>
-        {/* Render Child Avatars */}
-        {childData && childData.length > 0 && childData.map((child) => (
-          <div key={child.id} className='avatar flex flex-col cursor-pointer '>
-            <div className='avatar w-20 h-20 space-x-2 rounded-full border-2 overflow-hidden'>
-              <Link href={`/mainpage/profile/parent/child?userId=${userId}&childId=${child.id}`}>
-                <Image
-                  src={child.imageUrl}
-                  width={100}
-                  height={100}
-                  alt={child.avatarAlt || "Child's avatar"}
-                  className='w-full h-full object-cover'
-                />
-              </Link>
-            </div>
-            <p className='text-sm text-center mt-2 text-gray-400'>{child.name}</p>
+        {(!childData || childData.length === 0) ? (
+          <div className="text-left text-black rounded-lg">
+            <p>Please add child profile at Home</p>
+
           </div>
-        ))}
+        ):(
+          <div>
+            {childData.map((child) => (
+            <div key={child.id} className='avatar flex flex-col cursor-pointer '>
+              <div className='avatar w-20 h-20 space-x-2 rounded-full border-2 overflow-hidden'>
+                <Link href={`/mainpage/profile/parent/child?userId=${userId}&childId=${child.id}`}>
+                  <Image
+                    src={child.imageUrl}
+                    width={100}
+                    height={100}
+                    alt={child.avatarAlt || "Child's avatar"}
+                    className='w-full h-full object-cover'
+                  />
+                </Link>
+              </div>
+              <p className='text-sm text-center mt-2 text-gray-400'>{child.name}</p>
+            </div>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   )
